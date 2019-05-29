3165a86676f58045f7d8b8128b68970bad857d14
James Aguilar <799564+jaguilar@users.noreply.github.com>
2019-05-29 11:45:33 -0400

    Need to return rather than continue now that this is a separate function.

--- sync.js
+++ sync.js
@@ -15,7 +15,7 @@ export async function sync1(ns, filename) {
   const tmpfile = `tmp-${Math.floor(Math.random() * 100000)}.js`;
   await ns.wget(`https://raw.githubusercontent.com/jaguilar/bitburner_scripts/master/${filename}?disableCache={Math.floor(Math.random()*100000)}`, tmpfile);
   const content = ns.read(tmpfile);
-  if (content.length === 0) continue;
+  if (content.length === 0) return;
   if (content != ns.read(filename)) {
     console.debug("changed content, overwriting " + filename);
     ns.write(filename, content, "w");
