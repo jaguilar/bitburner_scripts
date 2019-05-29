e477e8c759c1c10a0667397037b68b8fdecd7e42
James Aguilar <799564+jaguilar@users.noreply.github.com>
2019-05-29 16:01:19 -0400

    Rewrite hacking mission in ts

--- hacking_mission.js
+++ hacking_mission.js
@@ -1,93 +1,101 @@
+import { Scheduler } from "scheduler.js";
 export async function main(ns) {
-  while (true) {
-    let board = readBoard();
-    console.debug(board);
-    await ns.sleep(10000);
-  }
+    let s = new Scheduler();
+    s.load();
+    s.load();
+    while (true) {
+        let board = readBoard();
+        console.debug(board);
+        await ns.sleep(10000);
+    }
 }
-
-class Board {
-  constructor() {
-    this.data = [];
-    this.data.length = 64;
-  }
-
-  get(y, x) {
-    return this.data[y * 8 + x];
-  }
-
-  set(y, x, u) {
-    this.data[y * 8 + x] = u;
-  }
-}
-
-const NODE_TYPE_CORE = 1;
-const NODE_TYPE_SPAM = 2;
-const NODE_TYPE_SHIELD = 3;
-const NODE_TYPE_TRANSFER = 4;
-const NODE_TYPE_DATABASE = 5;
-const NODE_TYPE_FIREWALL = 6;
+var NodeType;
+(function (NodeType) {
+    NodeType[NodeType["Core"] = 1] = "Core";
+    NodeType[NodeType["Spam"] = 2] = "Spam";
+    NodeType[NodeType["Shield"] = 3] = "Shield";
+    NodeType[NodeType["Transfer"] = 4] = "Transfer";
+    NodeType[NodeType["Database"] = 5] = "Database";
+    NodeType[NodeType["Firewall"] = 6] = "Firewall";
+})(NodeType || (NodeType = {}));
 function nodeTypeFromName(name) {
-  switch (name) {
-    case "CPU Core":
-      return NODE_TYPE_CORE;
-    case "Spam":
-      return NODE_TYPE_SPAM;
-    case "Shield":
-      return NODE_TYPE_SHIELD;
-    case "Transfer":
-      return NODE_TYPE_TRANSFER;
-    case "Database":
-      return NODE_TYPE_DATABASE;
-    case "Firewall":
-      return NODE_TYPE_FIREWALL;
-  }
+    switch (name) {
+        case "CPU Core":
+            return NodeType.Core;
+        case "Spam":
+            return NodeType.Spam;
+        case "Shield":
+            return NodeType.Shield;
+        case "Transfer":
+            return NodeType.Transfer;
+        case "Database":
+            return NodeType.Database;
+        case "Firewall":
+            return NodeType.Firewall;
+        default:
+            throw new Error("Unsupported type: " + name);
+    }
 }
-
-const NODE_OWNER_NEUTRAL = 1;
-const NODE_OWNER_ME = 2;
-const NODE_OWNER_ENEMY = 3;
+var NodeOwner;
+(function (NodeOwner) {
+    NodeOwner[NodeOwner["Me"] = 1] = "Me";
+    NodeOwner[NodeOwner["Enemy"] = 2] = "Enemy";
+    NodeOwner[NodeOwner["Neutral"] = 3] = "Neutral";
+})(NodeOwner || (NodeOwner = {}));
 function nodeOwnerFromClass(nodeClass) {
-  if (nodeClass.indexOf("player") != -1) return NODE_OWNER_ME;
-  if (nodeClass.indexOf("enemy") != -1) return NODE_OWNER_ENEMY;
-  return NODE_OWNER_NEUTRAL;
+    if (nodeClass.indexOf("player") != -1)
+        return NodeOwner.Me;
+    if (nodeClass.indexOf("enemy") != -1)
+        return NodeOwner.Enemy;
+    return NodeOwner.Neutral;
 }
-
 const nodeIdRe = /^.*-(\d+)-(\d+)$/;
 const nodeTextRe = /((?:CPU Core)|\w+)\s+HP: ((?:\.|\d)+)\sAtk: ((?:\.|\d)+)\s+Def: ((?:\.|\d)+)/m;
-
 class GridElement {
-  /**
-   *
-   * @param {HTMLElement} divNode
-   */
-  constructor(divNode) {
-    this.node = divNode;
-    const idMatch = nodeIdRe.exec(this.node.id);
-    this.y = parseInt(idMatch[1]);
-    this.x = parseInt(idMatch[2]);
-
-    this.owner = nodeOwnerFromClass(this.node.className);
-
-    const textMatch = nodeTextRe.exec(this.node.children[0].innerText);
-    this.type = nodeTypeFromName(textMatch[1]);
-    this.hp = parseFloat(textMatch[2]);
-    this.atk = parseFloat(textMatch[3]);
-    this.def = parseFloat(textMatch[4]);
-  }
+    constructor(node) {
+        this.node = node;
+        const idMatch = nodeIdRe.exec(this.node.id);
+        if (!idMatch) {
+            throw new Error(`Expected match from ${this.node.id} with ${nodeIdRe}`);
+        }
+        this.y = parseInt(idMatch[1]);
+        this.x = parseInt(idMatch[2]);
+        this.owner = nodeOwnerFromClass(this.node.className);
+        const textMatch = nodeTextRe.exec(this.node.innerText);
+        if (!textMatch) {
+            throw new Error(`Expected match from ${this.node.innerText} with ${nodeTextRe}`);
+        }
+        this.type = nodeTypeFromName(textMatch[1]);
+        this.hp = parseFloat(textMatch[2]);
+        this.atk = parseFloat(textMatch[3]);
+        this.def = parseFloat(textMatch[4]);
+    }
 }
-
+class Board {
+    constructor() {
+        this.data = [];
+        this.data = [];
+        this.data.length = 64;
+    }
+    get(y, x) {
+        return this.data[y * 8 + x];
+    }
+    set(y, x, u) {
+        this.data[y * 8 + x] = u;
+    }
+}
 function readBoard() {
-  const gridRoot = document.querySelectorAll("div .hack-mission-grid")[0];
-  if (!gridRoot) return;
-
-  const board = new Board();
-  for (const child of gridRoot.children) {
-    if (!child.id.startsWith("hacking-mission-node")) continue;
-
-    const gridElement = new GridElement(child);
-    board.set(gridElement.y, gridElement.x, gridElement);
-  }
-
-  return board;
+    const gridRoot = document.querySelectorAll("div .hack-mission-grid")[0];
+    if (!gridRoot)
+        return null;
+    const board = new Board();
+    for (const child of gridRoot.children) {
+        if (!child.id.startsWith("hacking-mission-node") ||
+            child.constructor != HTMLAnchorElement) {
+            continue;
+        }
+        const gridElement = new GridElement(child);
+        board.set(gridElement.y, gridElement.x, gridElement);
+    }
+    return board;
 }
---
+++ hacking_mission.ts
@@ -0,0 +1,119 @@
+import { Scheduler } from "scheduler.js";
+
+export async function main(ns: IGame) {
+  let s = new Scheduler();
+  s.load();
+  s.load();
+
+  while (true) {
+    let board = readBoard();
+    console.debug(board);
+    await ns.sleep(10000);
+  }
+}
+
+enum NodeType {
+  Core = 1,
+  Spam = 2,
+  Shield = 3,
+  Transfer = 4,
+  Database = 5,
+  Firewall = 6,
+}
+function nodeTypeFromName(name: string) {//
+  switch (name) {
+    case "CPU Core":
+      return NodeType.Core;
+    case "Spam":
+      return NodeType.Spam;
+    case "Shield":
+      return NodeType.Shield;
+    case "Transfer":
+      return NodeType.Transfer;
+    case "Database":
+      return NodeType.Database;
+    case "Firewall":
+      return NodeType.Firewall;
+    default:
+      throw new Error("Unsupported type: " + name);
+  }
+}
+
+enum NodeOwner {
+  Me = 1,
+  Enemy = 2,
+  Neutral = 3,
+}
+function nodeOwnerFromClass(nodeClass: string) {
+  if (nodeClass.indexOf("player") != -1) return NodeOwner.Me;
+  if (nodeClass.indexOf("enemy") != -1) return NodeOwner.Enemy;
+  return NodeOwner.Neutral;
+}
+
+const nodeIdRe = /^.*-(\d+)-(\d+)$/;
+const nodeTextRe = /((?:CPU Core)|\w+)\s+HP: ((?:\.|\d)+)\sAtk: ((?:\.|\d)+)\s+Def: ((?:\.|\d)+)/m;
+
+class GridElement {
+  public x: number;
+  public y: number;
+  public owner: NodeOwner;
+  public type: NodeType;
+  public hp: Number;
+  public atk: Number;
+  public def: Number;
+
+  constructor(public node: HTMLAnchorElement) {
+    const idMatch = nodeIdRe.exec(this.node.id);
+    if (!idMatch) {
+      throw new Error(`Expected match from ${this.node.id} with ${nodeIdRe}`);
+    }
+    this.y = parseInt(idMatch[1]);
+    this.x = parseInt(idMatch[2]);
+
+    this.owner = nodeOwnerFromClass(this.node.className);
+
+    const textMatch = nodeTextRe.exec(this.node.innerText);
+    if (!textMatch) {
+      throw new Error(`Expected match from ${this.node.innerText} with ${nodeTextRe}`);
+    }
+    this.type = nodeTypeFromName(textMatch[1]);
+    this.hp = parseFloat(textMatch[2]);
+    this.atk = parseFloat(textMatch[3]);
+    this.def = parseFloat(textMatch[4]);
+  }
+}
+
+class Board {
+  data: Array<GridElement> = [];
+
+  constructor() {
+    this.data = [];
+    this.data.length = 64;
+  }
+
+  get(y: number, x: number) {
+    return this.data[y * 8 + x];
+  }
+
+  set(y: number, x: number, u: GridElement) {
+    this.data[y * 8 + x] = u;
+  }
+}
+
+function readBoard(): Board | null {
+  const gridRoot = document.querySelectorAll("div .hack-mission-grid")[0];
+  if (!gridRoot) return null;
+
+  const board = new Board();
+  for (const child of gridRoot.children) {
+    if (!child.id.startsWith("hacking-mission-node") ||
+        child.constructor != HTMLAnchorElement) {
+      continue;
+    }
+
+    const gridElement = new GridElement(child as HTMLAnchorElement);
+    board.set(gridElement.y, gridElement.x, gridElement);
+  }
+
+  return board;
+}
